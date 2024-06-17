47b22a82818f43e2f23e08733c5c872d94de82a0
Morten Elk <morten@elk.dk>
2024-06-17 15:16:55 +0000

    day added to 02

---
+++ mmm/fbgoogle/02utilfbgoogle.py
@@ -0,0 +1,11 @@
+import json
+from pathlib import Path
+from pymc_marketing.mmm.delayed_saturated_mmm import DelayedSaturatedMMM
+import arviz as az
+
+
+class MelkDelayedSaturatedMMM(DelayedSaturatedMMM):
+    def dummy(self):
+        pass    
+
+    
